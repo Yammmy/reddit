cace7afc167698856938deaf4cd0c940985ec2aa
Yammy <yammyxing@foxmail.com>
2017-01-25 11:09:41 +0800

    ad association between link and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'links#index'
   resources :links
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
